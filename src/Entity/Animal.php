d736e254939e4c8a0a2456f9042636bb907c4e08
Murilo Nascimento <murilobsi2018@gmail.com>
2022-06-30 21:56:26 -0300

    FEAT - Add entity animal

---
+++ src/Entity/Animal.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnimalRepository::class)
+ */
+class Animal
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $leite;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $racao;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $peso;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $nascimento;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $situacao;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLeite(): ?float
+    {
+        return $this->leite;
+    }
+
+    public function setLeite(float $leite): self
+    {
+        $this->leite = $leite;
+
+        return $this;
+    }
+
+    public function getRacao(): ?float
+    {
+        return $this->racao;
+    }
+
+    public function setRacao(float $racao): self
+    {
+        $this->racao = $racao;
+
+        return $this;
+    }
+
+    public function getPeso(): ?float
+    {
+        return $this->peso;
+    }
+
+    public function setPeso(float $peso): self
+    {
+        $this->peso = $peso;
+
+        return $this;
+    }
+
+    public function getNascimento(): ?\DateTimeInterface
+    {
+        return $this->nascimento;
+    }
+
+    public function setNascimento(\DateTimeInterface $nascimento): self
+    {
+        $this->nascimento = $nascimento;
+
+        return $this;
+    }
+
+    public function getSituacao(): ?int
+    {
+        return $this->situacao;
+    }
+
+    public function setSituacao(int $situacao): self
+    {
+        $this->situacao = $situacao;
+
+        return $this;
+    }
+}
