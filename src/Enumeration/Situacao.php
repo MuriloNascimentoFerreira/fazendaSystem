eb80aaf9eb8b3ed02d198a591ec9025b0c441a17
Murilo Nascimento <murilobsi2018@gmail.com>
2022-07-01 11:51:39 -0300

    REFACTOR - Modificando metodo get

--- src/Enumeration/Situacao.php
+++ src/Enumeration/Situacao.php
@@ -7,12 +7,14 @@ class Situacao
     const VIVO = 'Vivo';
     const ABATIDO = 'Abatido';
 
-    public function getSituacao($numero){
-        switch($numero){
+    public function getSituacao($situacao){
+        switch($situacao){
             case 1: return Situacao::VIVO;
             case 2: return Situacao::ABATIDO;
+            case 'Vivo': return 1;
+            case 'Abatido': return 2;
         }
-        return $numero;
+        return $situacao;
     }
 }
 ?>
