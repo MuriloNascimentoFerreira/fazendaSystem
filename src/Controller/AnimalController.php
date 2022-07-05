806f9cebb0f132b4f6da8ed7082d73acd7af26de
Murilo Nascimento <murilobsi2018@gmail.com>
2022-07-05 18:35:14 -0300

    FEAT - Add edit CRUD

--- src/Controller/AnimalController.php
+++ src/Controller/AnimalController.php
@@ -18,6 +18,12 @@ use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class AnimalController extends AbstractController
 {
+
+    public function index(): Response
+    {
+        return $this->render('homePage.html.twig');
+    }
+
     public function listar($entityManager): array
     {
 
@@ -48,6 +54,7 @@ class AnimalController extends AbstractController
      */
     public function adicionar(Request $request,EntityManagerInterface $em) : Response
     {
+        $this->index();
         $animais = $this->listar($em);
         $animal = new Animal();
         $Situacao = new Situacao();
@@ -75,39 +82,36 @@ class AnimalController extends AbstractController
         $data['titulo'] = 'Adicionar Animal';
         $data['form'] = $form;
         $data['animais'] = $animais;
-
         return $this->renderForm('animal/form.html.twig',$data);
     }
 
     /**
      * @Route("/editar/{id}", name="animal_editar")
      */
-    public function editar(int $id,Request $request, AnimalRepository $animalRepository): Response
+    public function editar(int $id,Request $request, EntityManagerInterface $orm): Response
     {
-        $animal = $animalRepository->find($id);
-
+        $animal = $orm->getRepository(Animal::class)->find($id);
         $formulario = $this->createForm(AnimalType::class, $animal, ['attr'=> ['class' => 'row align-items-center d-inline-flex']]);
         $formulario->handleRequest($request);
 
         if($formulario->isSubmitted() && $formulario->isValid()){
             try{
-                $animalRepository->persist($animal);
-                $animalRepository->flush();
+                $orm->persist($animal);
+                $orm->flush();
                 $this->addFlash('success',"Animal Alterado com sucesso!");
-                sleep(4);
+            //    return $this->redirectToRoute("animal_editar",['id'=>$id]);
 
             }catch (\Exception $e){
                 $this->addFlash('erro','Falha ao Alterar o animal!');
-                sleep(4);
-                return $this->redirectToRoute("animal_adicionar");
+            //    return $this->redirectToRoute("animal_editar",['id'=>$id]);
             }
-
+            return $this->redirectToRoute("animal_adicionar");
         }
 
         $data['titulo'] = 'Editar Animal';
         $data['formulario'] = $formulario;
-        return $this->render('animal/editar.html.twig',$data);
 
+        return $this->renderForm('animal/editar.html.twig', $data);
     }
 
 }
